4c7d48b5adade90c4086cf5353e135ab75bc9191
David Fuentes <davidf0827@gmail.com>
2020-07-30 04:37:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.size == 1
+  if array.length == 1
     return array[0]
-  elsif array.size == 2
+  elsif array.length == 2
     return array.join(" and ")
   else
     return array[0..-2].join(', ') + ", and " + array[-1]
